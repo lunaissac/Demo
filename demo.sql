b94f94d8e78f239334a190e6c107d6d9ab3a0170
LunaGJ <iluna@vallegrande.edu.pe>
2019-01-23 09:30:37 -0500

    esto es un demo

--- demo.sql
+++ demo.sql
@@ -1,1 +1,18 @@
-demo issac super
+CREATE TABLE persona (
+   codper integer  NOT NULL,
+   nomper varchar2(100)  NULL,
+   apepatper varchar2(150)  NULL,
+   apematper varchar2(150)  NULL,
+   dniper varchar2(8)  NULL,
+   dirper varchar2(150)  NULL,
+   telef1per varchar2(9)  NULL,
+   telef2per varchar2(9)  NULL,
+   emailper varchar2(80)  NULL,
+   estper varchar2(1)  NULL,
+   usuarper integer  NULL,
+   passwper varchar2(80)  NULL,
+   sexoper varchar2(1)  NULL,
+   tipoper varchar2(1)  NULL,
+   codubi varchar2(6)  NOT NULL,
+   CONSTRAINT perosna_pk PRIMARY KEY (codper)
+) ;
